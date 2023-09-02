4386f07214c801fa5ebd44455f38d92663c2deb6
natukin1978 <natukin1978@hotmail.com>
2023-09-02 22:05:09 +0900

    Execute the string in the text file that serves as a template.

--- write_prompt.py
+++ write_prompt.py
@@ -1,13 +1,26 @@
 import sys
+import argparse
 from PIL import Image
 from PIL.PngImagePlugin import PngInfo
 
 
-def write_parameters(image_path, text_file_path):
+def replace_parameters(parameters, replace_from, replace_to):
+    # 文字列の置換を行います
+    parameters = parameters.replace(replace_from, replace_to)
+    return parameters
+
+
+def write_parameters(image_path, text_file_path, replace_from=None, replace_to=None):
     img = Image.open(image_path)
 
     with open(text_file_path, "r") as text_file:
         parameters = text_file.read().strip()
+
+    if not parameters:
+        return
+    # オプションの置換を行います
+    if replace_from and replace_to:
+        parameters = replace_parameters(parameters, replace_from, replace_to)
 
     metadata = PngInfo()
     metadata.add_text("parameters", parameters)
@@ -15,11 +28,20 @@ def write_parameters(image_path, text_file_path):
     print("Parameters written successfully.")
 
 
-# Example usage
-if len(sys.argv) != 3:
-    print("Usage: python write_prompt.py (image file path) (text file path)")
-    exit(1)
+def main():
+    parser = argparse.ArgumentParser(description="Write parameters to a PNG image.")
+    parser.add_argument("image_path", help="Path to the image file")
+    parser.add_argument(
+        "text_file_path", help="Path to the text file containing parameters"
+    )
+    parser.add_argument("--replace-from", help="Text to replace in the parameters")
+    parser.add_argument("--replace-to", help="Replacement text for the parameters")
 
-image_path = sys.argv[1]
-text_file_path = sys.argv[2]
-write_parameters(image_path, text_file_path)
+    args = parser.parse_args()
+    write_parameters(
+        args.image_path, args.text_file_path, args.replace_from, args.replace_to
+    )
+
+
+if __name__ == "__main__":
+    main()
